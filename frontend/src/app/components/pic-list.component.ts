bc3c224b714dfa799853da81cc5f0885fa5b56ca
linghong <linghong_chen@hotmail.com>
2016-11-03 12:23:35 -0400

    added pic-list component.

---
+++ frontend/src/app/components/pic-list.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pic-list',
+  templateUrl: './pic-list.component.html'
+})
+export class PicListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
